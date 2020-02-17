64ff78b923d3a9d0ab34c20c356e5ae6ce9b9543
Victor Caldas <victor.solid3@gmail.com>
2020-02-17 11:14:27 -0300

    refactoring the script

--- script.js
+++ script.js
@@ -15,42 +15,57 @@ function showError(input, message) {
 function checkEmail(email) {
   const regex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.text(String(email).toLocaleLowerCase());
+  if (regex.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not Valid');
+  }
 }
 /* Show Success Message */
 function showSuccess(input, message) {
   const formControl = input.parentElement;
   formControl.classList.add('success');
 }
-
-function submit(event) {
-  event.preventDefault();
-  if (username.value === '') {
-    showError(username, 'Username Required')
-  } else {
-    showSuccess(username)
-  }
-  if (email.value === '') {
-    showError(email, 'Email Required')
-  } else if (checkEmail(!email.value)) {
-    showError(email, 'Email is not valid');
-  }
-  else {
-    showSuccess(email)
-  }
-  if (password.value === '') {
-    showError(password, 'Password Required')
-  } else {
-    showSuccess(password)
-  }
-  if (password2.value === password) {
-    showError(password2, 'Password not match')
-  } else {
-    showSuccess(password2)
+/* Requirement Checking */
+function checkRequirement(inputArray) {
+  inputArray.forEach((input) => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+/* Checking MatchPasswords */
+function checkPassword(input1, input2) {
+  if (input1.value !== input.value) {
+    showError(input2, 'password do not match');
   }
 }
+/* Get FieldName */
+function getFieldName(input) {
+  /* Making the field captalized */
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+/* Check Input Lenght */
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
 
-
+}
 
 /* Event Listeners */
+function submit(event) {
+  event.preventDefault();
+  checkRequirement([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPassword(password, password2);
+}
 form.addEventListener('submit', submit);
